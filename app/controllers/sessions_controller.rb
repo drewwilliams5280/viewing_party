c80e282a5a68d8274e0e8a5a7c4dc10d77f0c8f8
Drew Williams <drewwilliams5280@gmail.com>
2020-10-06 16:53:22 -0600

    Adds log in functionality for current_user and tests for happy and sad paths. Adds user to seed file; researching FactoryBot fill ins.
    Co-authored-by: Drew Williams <65911652+drewwilliams5280@users.noreply.github.com>
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/dashboard'
+    else
+      flash[:error] = "Your login credentials are incorrect"
+      redirect_to '/'
+    end
+  end
+
+end
---
+++ spec/features/users/dashboard_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "As a visitor" do
+  it "I can log in" do
+    user = User.create(name: "Drewni Coliams", email: "drewnicoliams@email.com", password: "123")
+    visit '/'
+    fill_in "email", with: user.email
+    fill_in "password", with: "1234"
+    click_on "Log In"
+    expect(page).to have_content("Your login credentials are incorrect")
+    fill_in "email", with: user.email
+    fill_in "password", with: "123"
+    click_on "Log In"
+    expect(current_path).to eq("/dashboard")
+    expect(page).to have_content("Welcome #{user.name}!")
+  end
+
+  it "displays error message when not logged in and visiting dashboard" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(nil)
+    visit '/dashboard'
+    expect(page).to have_content("You are not authorized to access this page")
+    expect(page).to have_content("401")
+  end
+
+end
