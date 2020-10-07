17a6aacf691bb5803bb1a465118aa6202a88f893
Dani Coleman <danicoleman00@gmail.com>
2020-10-07 15:28:11 -0600

    Adds friendship relations to user
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Drew Williams 
    <65911652+drewwilliams5280@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :name
   validates_presence_of :password, require: true
 
-  has_secure_password
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
 
 end
