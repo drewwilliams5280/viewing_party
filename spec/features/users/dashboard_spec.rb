dcb44b7edb05748612bde24b1c81ae72dacdbc26
Dani Coleman <danicoleman00@gmail.com>
2020-10-07 07:51:51 -0600

    Adds a before each do to define user using faker info. Error for password needs resolved
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -1,23 +1,33 @@
 require "rails_helper"
 
 RSpec.describe "As a visitor" do
+  before :each do
+    user = create(:user)
+ #ActiveRecord::RecordInvalid Validation failed: Password can't be blank, Password can't be blank
+  end
   it "I can log in" do
-    user = User.create(name: "Drewni Coliams", email: "drewnicoliams@email.com", password: "123")
+    # user = User.create(name: "Drewni Coliams", email: "drewnicoliams@email.com", password: "123")
     visit '/'
-    fill_in "email", with: user.email
+    fill_in "email", with: @user.email
     fill_in "password", with: "1234"
+
     click_on "Log In"
+
     expect(page).to have_content("Your login credentials are incorrect")
-    fill_in "email", with: user.email
+
+    fill_in "email", with: @user.email
     fill_in "password", with: "123"
+
     click_on "Log In"
+
     expect(current_path).to eq("/dashboard")
-    expect(page).to have_content("Welcome #{user.name}!")
+    expect(page).to have_content("Welcome #{@user.name}!")
   end
 
   it "displays error message when not logged in and visiting dashboard" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(nil)
     visit '/dashboard'
+
     expect(page).to have_content("You are not authorized to access this page")
     expect(page).to have_content("401")
   end
