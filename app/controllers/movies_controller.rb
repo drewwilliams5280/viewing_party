c72579d19a8e363d95d0248fd6bcf1aa5e782e48
Drew Williams <drewwilliams5280@gmail.com>
2020-10-08 18:27:12 -0600

    Finishes movies page and test. MoviesController index method logically correct, needs refactoring and helper methods.
    Co-authored-by: Drew Williams <65911652+drewwilliams5280@users.noreply.github.com>
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,35 @@
 class MoviesController < ApplicationController
 
   def index
-    conn = Faraday.new(url: 'https://api.themoviedb.org')
-    response1 = conn.get('/3/movie/top_rated') do |faraday|
-      faraday.params['api_key'] = ENV['MOVIE_API_KEY']
-      faraday.params['page'] = '1'
+    if params[:q]
+      conn = Faraday.new(url: 'https://api.themoviedb.org')
+      response1 = conn.get('/3/movie/top_rated') do |faraday|
+        faraday.params['api_key'] = ENV['MOVIE_API_KEY']
+        faraday.params['page'] = '1'
+      end
+      response2 = conn.get('/3/movie/top_rated') do |faraday|
+        faraday.params['api_key'] = ENV['MOVIE_API_KEY']
+        faraday.params['page'] = '2'
+      end
+      json1 = JSON.parse(response1.body, symbolize_names: true)
+      json2 = JSON.parse(response2.body, symbolize_names: true)
+      @movies = (json1[:results] + json2[:results]).flatten
+    elsif params[:search]
+      conn = Faraday.new(url: 'https://api.themoviedb.org')
+      response1 = conn.get('/3/search/movie') do |faraday|
+        faraday.params['api_key'] = ENV['MOVIE_API_KEY']
+        faraday.params['query'] = params[:search]
+        faraday.params['page'] = "1"
+      end
+      response2 = conn.get('/3/search/movie') do |faraday|
+        faraday.params['api_key'] = ENV['MOVIE_API_KEY']
+        faraday.params['query'] = params[:search]
+        faraday.params['page'] = "2"
+      end
+      json1 = JSON.parse(response1.body, symbolize_names: true)
+      json2 = JSON.parse(response2.body, symbolize_names: true)
+      @movies = (json1[:results] + json2[:results]).flatten
     end
-    response2 = conn.get('/3/movie/top_rated') do |faraday|
-      faraday.params['api_key'] = ENV['MOVIE_API_KEY']
-      faraday.params['page'] = '2'
-    end
-    json1 = JSON.parse(response1.body, symbolize_names: true)
-    json2 = JSON.parse(response2.body, symbolize_names: true)
-    @movies = (json1[:results] + json2[:results]).flatten
   end
 
 end
--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -7,23 +7,37 @@ RSpec.describe "Movies index page" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
-    it "has correct buttons", :vcr do
-      visit '/movies'
-      expect(page).to have_button("Find Top Rated Movies")
-      expect(page).to have_field("Search by movie title")
-      expect(page).to have_button("Find Movies")
-    end
-
     it "lists top 40 movies", :vcr do
       visit '/discover'
       click_on "Find Top Rated Movies"
+      expect(page).to have_button("Find Top Rated Movies")
+      expect(page).to have_field("Search by movie title")
+      expect(page).to have_button("Find Movies")
       expect(current_path).to eq(movies_path)
       expect(page).to have_css(".movie")
 
-      within(first('#movie')) do
+      within(first('.movie')) do
         expect(page).to have_css('.title')
         expect(page).to have_css('.vote_average')
       end
+    end
+
+    it "can list search movies", :vcr do
+      visit '/discover'
+      fill_in "Search by movie title", with: "dragon"
+      click_on "Find Movie"
+      expect(page).to have_button("Find Top Rated Movies")
+      expect(page).to have_field("Search by movie title")
+      expect(page).to have_button("Find Movies")
+      expect(current_path).to eq(movies_path)
+      expect(page).to have_css(".movie")
+
+      within(first('.movie')) do
+        expect(page).to have_content("Dragon")
+        expect(page).to have_css('.title')
+        expect(page).to have_css('.vote_average')
+      end
+
     end
   end
 end
