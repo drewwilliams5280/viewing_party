f21dfe4c9c67eb8f1abe2d33b336ff2a8e2a1cfd
Dani Coleman <danicoleman00@gmail.com>
2020-10-14 07:14:43 -0600

    Add users and friendships to seeds
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Drew Williams 
    <65911652+drewwilliams5280@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(name: "Drewni Coliams", email: "drewnicoliams@email.com", password: "123")
+drewni = User.create(name: "Drewni Coliams", email: "drewnicoliams@email.com", password: "123")
+hank = User.create(name: "Hank Hill", email: "hank@email.com", password: "123")
+homer = User.create(name: "Homer Simpson", email: "homer@email.com", password: "123")
+
+friendship1 = Friendship.create(friend: drewni, user: hank)
+friendship2 = Friendship.create(friend: hank, user: drewni)
+
+friendship3 = Friendship.create(friend: drewni, user: homer)
+friendship4 = Friendship.create(friend: homer, user: drewni)
