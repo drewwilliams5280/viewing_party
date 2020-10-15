c8fd642cd2e68b6ace51b3357726f74f0cd6172b
Dani Coleman <danicoleman00@gmail.com>
2020-10-15 08:57:55 -0600

    Fix rubocop errors
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Drew Williams 
    <65911652+drewwilliams5280@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    if user = User.find_by(email: params[:email])
+    if (user = User.find_by(email: params[:email]))
       if user.authenticate(params[:password])
         session[:user_id] = user.id
         flash[:success] = "Success, you are now logged in as #{user.name}"
@@ -10,14 +10,14 @@ class SessionsController < ApplicationController
         redirect_to root_path
       end
     else
-      flash[:error] = "Your login credentials are incorrect"
+      flash[:error] = 'Your login credentials are incorrect'
       redirect_to root_path
     end
   end
 
   def destroy
     session.delete(:user_id)
-    flash[:notice] = "You have been logged out"
+    flash[:notice] = 'You have been logged out'
     redirect_to root_path
   end
 end
